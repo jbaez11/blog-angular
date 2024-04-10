ec382b8372e8326d3a00b639d3eb3f1d8863b7e9
Jonathan Baez <jbaez9910@gmail.com>
2024-04-10 18:28:48 -0500

    feat: privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EntryComponent } from './entry/entry.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'entry/:urlPost/:postId/:createdAt', component: EntryComponent}
+  { path: 'entry/:urlPost/:postId/:createdAt', component: EntryComponent},
+  { path: 'privacy-policy', component: PrivacyPolicyComponent}
+
+
   // Otras rutas si las tienes
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import { HttpClientModule } from '@angular/common/http';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PrivacyPolicyComponent,
   ],
   imports: [
     BrowserModule,
